f0cf815d73cf3c526a083138511e01fae96313ab
fengjianchao <1422720954@qq.com>
2019-07-08 15:19:49 +0800

    add Bonjour.java

---
+++ src/main/java/cn/jc/javawebtutorial/Bonjour.java
@@ -0,0 +1,20 @@
+package cn.jc.javawebtutorial;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/bonjour")
+@RestController
+public class Bonjour {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "Get Bonjour Method";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String postMethod() {
+        return "Post Bonjour Method";
+    }
+}
