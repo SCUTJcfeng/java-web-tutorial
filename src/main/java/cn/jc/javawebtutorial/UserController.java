83c21459f52bdb991435754d17f048d71ba68e31
fengjianchao <1422720954@qq.com>
2019-07-11 12:18:17 +0800

    add userinfo route for @Service test

--- src/main/java/cn/jc/javawebtutorial/UserController.java
+++ src/main/java/cn/jc/javawebtutorial/UserController.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import cn.jc.javawebtutorial.modal.UserModal;
+import cn.jc.javawebtutorial.service.UserInfoService;
 
 class VipUser implements Serializable {
 
@@ -62,14 +64,44 @@ class VipUser implements Serializable {
     }
 }
 
+
 @RestController
 public class UserController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/user", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getUserList() {
         ArrayList<VipUser> list = new ArrayList<>();
         list.add(new VipUser("jack", 18));
         list.add(new VipUser("jackson", 11));
         return JSON.toJSONString(list);
+    }
+}
+
+
+@RestController
+class UserInfoController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @RequestMapping(value = "/userinfo", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getUserInfo(@RequestParam(value = "name", required = true) String name,
+            @RequestParam(value = "age", required = true) Integer age,
+            @RequestParam(value = "gender", required = true) Boolean gender) {
+        UserModal user = new UserModal();
+        user.setName(name);
+        user.setAge(age);
+        user.setGender(gender);
+        // return JSON.toJSONString(user);
+        Integer newAge = userInfoService.getUserAge(user);
+        String newName = userInfoService.getUserName(user);
+        Boolean newGender = userInfoService.getUserGender(user);
+        Map<String, Object> m = new HashMap<>();
+        m.put("name", newName);
+        m.put("age", newAge);
+        m.put("gender", newGender);
+        return JSON.toJSONString(m);
     }
 }
